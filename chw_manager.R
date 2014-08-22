323bf5db01889e19c025756154b9ca0c279aca1d
Emmanuel Toko <emmanuel.toko@gmail.com>
2014-08-22 14:01:43 +0300

    Added data.frame merge ability

--- chw_manager.R
+++ chw_manager.R
@@ -17,7 +17,7 @@ pvisit$form.meta.timeEnd <- as.Date(pvisit$form.meta.timeEnd)
 
 reportingPeriodEnd <- as.Date("2014-07-31")
 #Visits in last 42 days (six weeks)
-reportingPeriodStart <- reportingPeriodStart - 42 
+reportingPeriodStart <- reportingPeriodEnd - 42 
 last42DaysVisits <- pvisit[pvisit$form.meta.timeEnd >= reportingPeriodStart & 
                                pvisit$form.meta.timeEnd <= reportingPeriodEnd,]
 
@@ -26,7 +26,7 @@ last42DaysUniqueVisits <- last42DaysVisits[
     !duplicated(last42DaysVisits$form.case..case_id, fromLast = TRUE),]
 totalUniqueVisits <- nrow(last42DaysUniqueVisits)
 
-#DENOMINATOR
+#DENOMINATOR#####
 case_list <- read.csv("Cases.csv", na.strings = "---")
 pregnant_women <- case_list[case_list$type == "pregnancy",]
 pregnant_women$closed_on <- as.Date(pregnant_women$closed_on)
@@ -48,3 +48,9 @@ pregnant_women <- case_list[case_list$type == "pregnancy",]
 #case_list$d.edd <- as.Date(case_list$d.edd)
 case_list$d.edd <- as.Date(case_list$d.edd, format="%y-%m-%d")
 
+
+chw_table <- table(pregnant_women$username)
+chw_df <- as.data.frame(chw_table)
+unique_visits <- table(last42DaysUniqueVisits$form.meta.username)
+unique_visits <- as.data.frame(unique_visits)
+final <- merge(chw_df, unique_visits, by = c("Var1"))
